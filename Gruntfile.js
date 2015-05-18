0a67007dc46c70061b36f7231e2dc473b4cbaf05
Scott Bromander <scott@primeacademy.io>
2015-05-18 09:41:54 -0500

    Includes app.min.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
             build: {
-                src: 'client/app.js',
+                src: 'client//scripts/app.js',
                 dest: 'public/javascripts/app.min.js'
             }
         },
